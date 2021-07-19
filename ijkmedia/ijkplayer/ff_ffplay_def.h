c666c3c14483857e138b5017116424024e629828
hswu3 <hswu3@iflytek.com>
2021-07-19 09:34:58 +0800

    更新构建环境

--- ijkmedia/ijkplayer/ff_ffplay_def.h
+++ ijkmedia/ijkplayer/ff_ffplay_def.h
@@ -65,7 +65,6 @@
 #include "ff_ffmsg_queue.h"
 #include "ff_ffpipenode.h"
 #include "ijkmeta.h"
-#include "ijkavformat/ijklas.h"
 
 #define DEFAULT_HIGH_WATER_MARK_IN_BYTES        (256 * 1024)
 
@@ -721,8 +720,7 @@ typedef struct FFPlayer {
     char *mediacodec_default_name;
     int ijkmeta_delay_init;
     int render_wait_start;
-    int is_manifest;
-    LasPlayerStatistic las_player_statistic;
+
 
     /*用于录像截图*/
     AVFormatContext *m_ofmt_ctx;        // 用于输出的AVFormatContext结构体
@@ -848,7 +846,6 @@ inline static void ffp_reset_internal(FFPlayer *ffp)
     ffp->mediacodec_default_name        = NULL; // option
     ffp->ijkmeta_delay_init             = 0; // option
     ffp->render_wait_start              = 0;
-    ffp->is_manifest                    = 0;
 
     ijkmeta_reset(ffp->meta);
 
